b2d4c2247de7de8cde2f26608926b8a8ed9ad86c
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-05 11:25:47 +0100

    Stage #2 completed
    Stage #2 User is a gamemaster
    Now it’s time to analyze user input and print the state of the field depending on it. You’ll learn to address specific positions in a string to achieve the required outcome.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,16 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().replace("\"", "");
+        char[] cells = input.toCharArray();
+        System.out.println("---------");
+        System.out.println("|" + " " + cells[0] + " " + cells[1] + " " + cells[2] + " " + "|");
+        System.out.println("|" + " " + cells[3] + " " + cells[4] + " " + cells[5] + " " + "|");
+        System.out.println("|" + " " + cells[6] + " " + cells[7] + " " + cells[8] + " " + "|");
+        System.out.println("---------");
     }
 }
